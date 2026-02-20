553a5cba97b32ab6ba3e7ed3f799da1e2b1e06af
jadielalmeidan-prog <jadielalmeidan@gmail.com>
2026-02-19 21:16:46 -0300

    Add files via upload

--- mat1.py
+++ mat1.py
@@ -1,14 +1,18 @@
-dec1 = input("Vc deseja fazer uma conta?").lower()
-if dec1 in ("sim","s"):
+def decisão(conta):
 
-    while True:
-        num = float(eval(input(">>>")))
-        print(num)
-        dec2 = input("Vc deseja fazer outra conta?").lower()
-        if dec2 in("sim","s"):
-            continue
-        else:
-            print("Ok,então!!!")
-            break
-else:
-    print("Ok então...")
+    dec1 = input("Vc deseja fazer uma conta?\n").lower()
+    if dec1 in ("sim","s"):
+
+        while True:
+            num = float(eval(input(">>>")))
+            print(f"{num:.2f}")
+            dec2 = input("Vc deseja fazer outra conta?\n").lower()
+            if dec2 in("sim","s"):
+                continue
+            else:
+                print("Ok,então!!!")
+                break
+    else:
+        print("Ok então...")
+
+print(decisão(conta=0))
